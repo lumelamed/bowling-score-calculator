450bac075dd2d12615785e9da7c750a48ffd0abf
lumelamed <lumelamed@gmail.com>
2025-11-26 20:41:00 -0300

    initial-project

---
+++ BowlingScoreCalculator/tests/BowlingScoreCalculator.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BowlingScoreCalculator.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
